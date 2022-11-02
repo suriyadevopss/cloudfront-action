5cd7a4eb26ce9ea28a17e17b5642bbf91cc39fed
suriyadevopss <suriya.s@digitalmission.plus>
2022-11-02 15:09:03 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.8
+
+LABEL "com.github.actions.name"="S3 Sync"
+LABEL "com.github.actions.description"="Sync a directory to an AWS S3 repository"
+LABEL "com.github.actions.icon"="refresh-cw"
+LABEL "com.github.actions.color"="green"
+
+LABEL repository="https://github.com/suriya/s3-sync-action"
+
+
+# https://github.com/aws/aws-cli/blob/master/CHANGELOG.rst
+ENV AWSCLI_VERSION='1.18.14'
+
+
+RUN pip install --quiet --no-cache-dir awscli==${AWSCLI_VERSION}
+
+ADD entrypoint.sh /entrypoint.sh
+RUN chmod +x entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$DISTRIBUTION_ID" ]; then
+  echo "DISTRIBUTION_ID is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_SESSION_TOKEN" ]; then
+  echo "AWS_SESSION_TOKEN is not set. Quitting."
+  exit 1
+fi
+
+# Default to us-east-1 if AWS_REGION not set.
+if [ -z "$AWS_REGION" ]; then
+  AWS_REGION="us-east-1"
+fi
+
+
+sh -c "aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --paths '/*' "
